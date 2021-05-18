42d91a11e8204f1c9ad31f1e7dde3dc0d00b8b6e
andreyliporage <andreyliporage@gmail.com>
2021-05-18 11:43:52 -0300

    logout

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from 'src/app/models/user';
 import { Security } from 'src/app/utils/security.util';
 
@@ -10,10 +11,15 @@ export class NavbarComponent implements OnInit {
 
   public user: User | null = null;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     this.user = Security.getUser();
+  }
+
+  logout() {
+    Security.clear();
+    this.router.navigate(['/login']);
   }
 
 }
